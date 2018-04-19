6a063d8b12abcbdd75b6124fd3b6c0a2af69a773
Michael Boroski <mjboroski@gmail.com>
2018-04-19 19:54:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker(name)
-  return "Hello, my name is ${name}."
+  return "Hello, my name is #{$}{name}."
 end
 
 def batch_badge_creator
@@ -10,6 +10,8 @@ def assign_rooms
 
 end
 
-def printer
+def printer 
 
 end
+
+
