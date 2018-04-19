f8f8bcc0474d81a690497737d41f9c38671dcb8f
Michael Boroski <mjboroski@gmail.com>
2018-04-19 20:09:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(names)
   rooms=Array.new
-  names.each {|name| rooms.unshift("Hello, #{name}! You'll be assigned to room #{rooms.index+1}!")}
+  names.each {|name| rooms.unshift("Hello, #{name}! You'll be assigned to room #{rooms.index}!")}
   return rooms
 end
 
